4f46f1127da5dd3b61aa122d09cb9ae8d3babc1a
Jefferson Peña <jefferson152530@gmail.com>
2024-08-19 00:28:19 -0500

    First

---
+++ frmPrincipal.vb
@@ -0,0 +1,35 @@
+﻿Public Class frmPrincipal
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.IsMdiContainer = True
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Dim ObjArbitro As New frmArbitro
+        ObjArbitro.MdiParent = Me
+        ObjArbitro.Show()
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+        Me.LayoutMdi(2)
+    End Sub
+
+    Private Sub ToolStripButton5_Click(sender As Object, e As EventArgs) Handles ToolStripButton5.Click
+        Me.LayoutMdi(1)
+    End Sub
+
+    Private Sub ToolStripButton6_Click(sender As Object, e As EventArgs) Handles ToolStripButton6.Click
+        Me.LayoutMdi(0)
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        Dim ObjJugador As New frmJugador
+        ObjJugador.MdiParent = Me
+        ObjJugador.Show()
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        Dim ObjSolicitud As New frmSolicitud
+        ObjSolicitud.MdiParent = Me
+        ObjSolicitud.Show()
+    End Sub
+End Class
